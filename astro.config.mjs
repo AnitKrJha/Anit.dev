83f66201fc0612e08e5a29589f2a8d8c94a155f9
anitkrjha <anitkjha31@gmail.com>
2023-09-01 01:28:58 +0530

    upgrade to astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-    viewTransitions: true,
-  },
   integrations: [tailwind()],
 });
