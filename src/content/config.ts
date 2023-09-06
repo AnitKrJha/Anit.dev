db4a5e616db90b6e01addb7298c82c974d6b0e74
anitkrjha <anitkjha31@gmail.com>
2023-09-06 21:33:55 +0530

    feat: revamped project caard and some minor tweaks

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,7 @@ const blogCollection = defineCollection({
         date: z.string().transform((str) => new Date(str)),
     })
 });
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
