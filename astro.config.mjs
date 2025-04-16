a366472e67be3d2079c7c7f7d6b7de84f960c525
Anit <anitkjha31@gmail.com>
2025-04-17 01:29:26 +0530

    feat: added google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,17 @@ import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   image: {
-    domains: [
-      "dev-to-uploads.s3.amazonaws.com",
-      "https://dev-to-uploads.s3.amazonaws.com/",
-      "https://dev-to-uploads.s3.amazonaws.com/uploads/articles/psxvdn3z7a17rof1xn0j.png",
-      "og.anit.dev",
-    ],
+    domains: ["dev-to-uploads.s3.amazonaws.com", "https://dev-to-uploads.s3.amazonaws.com/", "https://dev-to-uploads.s3.amazonaws.com/uploads/articles/psxvdn3z7a17rof1xn0j.png", "og.anit.dev"]
   },
   site: "https://anit.dev",
-  integrations: [tailwind(), sitemap(), mdx(), prefetch()],
+  integrations: [tailwind(), sitemap(), mdx(), prefetch(), partytown()],
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
